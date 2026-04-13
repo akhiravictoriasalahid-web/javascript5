619bd5c600b72c4021506f0ad5dbf922db1919de
akhiravictoriasalahid-web <akhiravictoriasalahid@gmail.com>
2026-04-13 19:36:16 +0200

    Add ATM function with user input

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
 const account = {
     accountName: "Junie",
-    balance: 0,
+    balance: 1000,
 
     getBalance() {
         console.log("Balance: " + this.balance);
@@ -32,8 +32,53 @@ const account = {
     }
 };
 
-account.deposit(400);
-account.withdrawal(500);
-account.getBalance(0);
-account.getAccountName();
-account.exitAccount();
+//account.deposit(1000);
+//account.withdrawal(500);
+//account.getBalance(0);
+//account.getAccountName();
+//account.exitAccount();
+
+//ATM function
+function atm() {
+    const message = parseFloat(
+        prompt("Select a choice: 1) Balance 2) Deposit 3) Withdraw 4) Name 5) Exit")
+    );
+
+    switch (message) {
+        case 1:
+            account.getBalance();
+            break;
+
+        case 2:
+            let depositAmount = parseFloat(prompt("Enter amount to deposit"));
+            if (isNaN(depositAmount) || depositAmount <= 0) {
+                account.accountError("Invalid deposit amount");
+            } else {
+                account.deposit(depositAmount);
+            }
+            break;
+
+        case 3:
+            let withdrawAmount = parseFloat(prompt("Enter amount to withdraw"));
+            if (isNaN(withdrawAmount) || withdrawAmount <= 0) {
+                account.accountError("Invalid withdraw amount");
+            } else {
+                account.withdrawal(withdrawAmount);
+            }
+            break;    
+
+        case 4:
+            account.getAccountName();
+            break;
+
+        case 5:
+            account.exitAccount();
+            break;
+
+        default:
+            account.accountError("Invalid choice");
+    }
+}
+atm();
+
+    
